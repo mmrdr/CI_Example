a7ab5cff9c1f9d153fae57ddcbad2856e78637e4
mmrdr <isaev2k18@gmail.com>
2025-10-04 22:39:35 +0300

    Init commit

---
+++ CI_Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CI_Example
+//
+//  Created by Кирилл Исаев on 04.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
